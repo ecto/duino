d931ce3b31d613637deb11428467fa8a3f0c45f2
Cam Pedersen <diffference@gmail.com>
2011-12-01 16:48:07 -0500

    [major] add autodetection of Arduino

---
+++ node_modules/serialport/tests/test_write.js
@@ -0,0 +1,9 @@
+// Test with the epic VirtualSerialPortApp - http://code.google.com/p/macosxvirtualserialport/
+
+var SerialPort = require("../serialport").SerialPort;
+var sys = require("sys");
+
+var serial_port = new SerialPort("/dev/master", {baudrate: 9600});
+
+serial_port.write("It worked!\r");
+serial_port.close();
--- test/basic.js
+++ test/basic.js
@@ -3,4 +3,6 @@ var arduino = require('../');
 
 var board = new arduino.Board();
 
-board.write('HELLO WORLD');
+board.on('connected', function(){
+  board.write('HELLO WORLD');
+});
