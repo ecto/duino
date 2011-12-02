27eb51320dcfcf0f8771943a28ba746541cdf8c8
Cam Pedersen <diffference@gmail.com>
2011-12-02 03:34:56 -0500

    [major] pare down led example after lib refactor

--- test/led.js
+++ test/led.js
@@ -1,23 +1,9 @@
 var arduino = require('../'),
     board = new arduino.Board();
 
-var on = true;
-
-board.on('connected', function(){
-  setTimeout(function(){
-    board.pinMode(13, 'out');
-  }, 800);
-  setInterval(function(){
-    if (on) {
-      board.digitalWrite(13, '01');
-      on = false;
-    } else {
-      board.digitalWrite(13, '00');
-      on = true;
-    }
-  }, 1000);
+var led = new arduino.Led({
+  board: board,
+  pin: 13
 });
 
-board.on('message', function(data) {
-  console.log('  ' + data);
-});
+led.blink();
