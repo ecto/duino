c7f9de4284ee89e127263b0a99cc34f6a274048d
Cam Pedersen <diffference@gmail.com>
2011-12-07 20:19:13 -0500

    [feat] begin button lib

---
+++ examples/button.js
@@ -0,0 +1,18 @@
+var arduino = require('../');
+
+var board = new arduino.Board({
+  debug: true
+});
+
+var button = new arduino.Button({
+  board: board,
+  pin: 2
+});
+
+button.on('down', function(){
+  console.log('DOWN');
+});
+
+button.on('up', function(){
+  console.log('UP');
+});
--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 
 module.exports = {
-  Board: require('./lib/board'),
-  Led: require('./lib/led'),
-  Piezo: require('./lib/piezo')
+  Board:  require('./lib/board'),
+  Led:    require('./lib/led'),
+  Piezo:  require('./lib/piezo'),
+  Button: require('./lib/button')
 }
