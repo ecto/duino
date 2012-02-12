78c16a2c3bd4316817c9d0bc95873e7ee32c3f16
Jarrett Hawrylak <jjhawrylak+git@gmail.com>
2012-02-12 15:06:13 -0500

    Refactoring, added analog functions

--- src/du.ino
+++ src/du.ino
@@ -1,10 +1,10 @@
 #include <Servo.h>
 
-char messageBuffer[11];
+char messageBuffer[12];
 int index = 0;
 char cmd[3];
 char pin[3];
-char val[3];
+char val[4];
 char aux[4];
 bool debug = false;
 Servo servo;
@@ -32,25 +32,25 @@ void process() {
   cmd[2] = '\0';
   strncpy(pin, messageBuffer + 2, 2);
   pin[2] = '\0';
-  strncpy(val, messageBuffer + 4, 2);
-  val[2] = '\0';
-  strncpy(aux, messageBuffer + 6, 3);
+  strncpy(val, messageBuffer + 4, 3);
+  val[3] = '\0';
+  strncpy(aux, messageBuffer + 7, 3);
   aux[3] = '\0';
   
   if (debug) {
     Serial.println(messageBuffer);
   }
+  int cmdid = atoi(cmd);
   
-  if (strcmp(cmd, "00") == 0) {
-      sm(pin, val);
-  } else if (strcmp(cmd, "01") == 0) {
-      dw(pin, val); // digital write
-  } else if (strcmp(cmd, "02") == 0) {
-      dr(pin, val); //  digital read
-  } else if (strcmp(cmd, "98") == 0) {
-      handleServo(pin, val, aux);
-  } else if (strcmp(cmd, "99") == 0) {
-      toggleDebug(val);
+  switch(cmdid) {
+    case 0:  sm(pin,val);              break;
+    case 1:  dw(pin,val);              break;
+    case 2:  dr(pin,val);              break;
+    case 3:  aw(pin,val);              break;
+    case 4:  ar(pin,val);              break;
+    case 98: handleServo(pin,val,aux); break;
+    case 99: toggleDebug(val);         break;
+    default:                           break;
   }
 }
 
@@ -58,7 +58,7 @@ void process() {
  * Toggle debug mode
  */
 void toggleDebug(char *val) {
-  if (strcmp(val, "00") == 0) {
+  if (atoi(val) == 0) {
     debug = false;
     Serial.println("goodbye");
   } else {
@@ -72,8 +72,9 @@ void toggleDebug(char *val) {
  */
 void sm(char *pin, char *val) {
   if (debug) Serial.println("sm");
-  int p = atoi(pin);
-  if (strcmp(val, "00") == 0) {
+  int p = getPin(pin);
+  if(p == -1) { if(debug) Serial.println("badpin"); return; }
+  if (atoi(val) == 0) {
     pinMode(p, OUTPUT);
   } else {
     pinMode(p, INPUT);
@@ -85,9 +86,10 @@ void sm(char *pin, char *val) {
  */
 void dw(char *pin, char *val) {
   if (debug) Serial.println("dw");
-  int p = atoi(pin);
-  pinMode(p, OUTPUT);
-  if (strcmp(val, "00") == 0) {
+  int p = getPin(pin);
+  if(p == -1) { if(debug) Serial.println("badpin"); return; }
+  //pinMode(p, OUTPUT);
+  if (atoi(val) == 0) {
     digitalWrite(p, LOW);
   } else {
     digitalWrite(p, HIGH);
@@ -99,17 +101,60 @@ void dw(char *pin, char *val) {
  */
 void dr(char *pin, char *val) {
   if (debug) Serial.println("dr");
-  int p = atoi(pin);
-  pinMode(p, INPUT);
+  int p = getPin(pin);
+  if(p == -1) { if(debug) Serial.println("badpin"); return; }
+  //pinMode(p, INPUT);
   int oraw = digitalRead(p);
-  char m[4];
-  char out[4];
-  sprintf(m, "%d", p);
-  sprintf(out, "%d", oraw);
-  strcat(m, "::");
-  strcat(m, out);
+  char m[7];
+  sprintf(m, "%02d::%02d", p,oraw);
   Serial.println(m);
 }
+
+/*
+ * Analog read
+ */
+void ar(char *pin, char *val) {
+  if(debug) Serial.println("ar");
+  int p = getPin(pin);
+  if(p == -1) { if(debug) Serial.println("badpin"); return; }
+  //pinMode(p, INPUT); // don't want to sw
+  int rval = analogRead(p);
+  char m[8];
+  sprintf(m, "%s::%03d", pin, rval);
+  Serial.println(m);
+}  
+
+void aw(char *pin, char *val) {
+  if(debug) Serial.println("aw");
+  int p = getPin(pin);
+  if(p == -1) { if(debug) Serial.println("badpin"); return; }
+  analogWrite(p,atoi(val));
+}
+
+int getPin(char *pin) { //Converts to A0-A5, and returns -1 on error
+  int ret = -1;
+  if(pin[0] == 'A' || pin[0] == 'a')
+  {
+    switch(pin[1])
+    {
+      case '0':  ret = A0; break;
+      case '1':  ret = A1; break;
+      case '2':  ret = A2; break;
+      case '3':  ret = A3; break;
+      case '4':  ret = A4; break;
+      case '5':  ret = A5; break;
+      default:             break;
+    }
+  } else {
+    ret = atoi(pin);
+    if(ret == 0 && (pin[0] != '0' || pin[1] != '0'))
+    {
+      ret = -1;
+    }
+  }
+  return ret;
+}
+  
 
 /*
  * Handle Servo commands
@@ -117,14 +162,15 @@ void dr(char *pin, char *val) {
  */
 void handleServo(char *pin, char *val, char *aux) {
   if (debug) Serial.println("ss");
-  int p = atoi(pin);
+  int p = getPin(pin);
+  if(p == -1) { if(debug) Serial.println("badpin"); return; }
   Serial.println("got signal");
-  if (strcmp(val, "00") == 0) {
+  if (atoi(val) == 0) {
     servo.detach();
-  } else if (strcmp(val, "01") == 0) {
+  } else if (atoi(val) == 1) {
     servo.attach(p);
     Serial.println("attached");
-  } else if (strcmp(val, "02") == 0) {
+  } else if (atoi(val) == 2) {
     Serial.println("writing to servo");
     Serial.println(atoi(aux));
     servo.write(atoi(aux));
