6f59b13c8f7b0fc74aba6ec6728cf0d2987453ae
Rick Waldron waldron.rick@gmail.com <waldron.rick@gmail.com>
2012-03-21 14:31:07 -0400

    Adds links to confirmed/tested sensors
    Signed-off-by: Rick Waldron waldron.rick@gmail.com <waldron.rick@gmail.com>

--- examples/sensor.js
+++ examples/sensor.js
@@ -15,3 +15,11 @@ ldr.on('read', function(value) {
   // |value| is reading of the light dependent resistor
   console.log(value);
 });
+
+// Tested with:
+// SoftPot
+//  http://www.spectrasymbol.com/how-it-works-softpot
+//  http://www.sparkfun.com/datasheets/Sensors/Flex/SoftPot-Datasheet.pdf
+//
+// LDR
+//  http://www.ladyada.net/learn/sensors/cds.html
